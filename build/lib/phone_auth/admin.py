c8a05e2fccb865232bac113b030a76b192518f0e
Samyak Jain <samyakjain101@gmail.com>
2021-03-21 16:45:39 +0530

    ignorefiles

--- build/lib/phone_auth/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import PhoneNumber
-
-
-admin.site.register(PhoneNumber)
--- build/lib/phone_auth/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PhoneAuthConfig(AppConfig):
-    name = 'phone_auth'
